49273c574191c42455d700a2e63b294e7998e664
R-azy <muhammedrazy1798@gmail.com>
2025-01-29 10:41:05 +0530

    added index.js

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+require('dotenv').config();
+
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
+
+// Export the config object
+module.exports = config;
+
+// Log the variables (Optional for testing)
+console.log(config);
